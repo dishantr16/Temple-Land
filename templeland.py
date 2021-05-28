854fa590fbcbc7bc1c2ca144cf871bac7a475fe1
Dishant Rathi <dishant16rathi@gmail.com>
2021-05-28 15:35:56 +0530

    temple land

---
+++ templeland.py
@@ -0,0 +1,27 @@
+for _ in range(int(input())):
+    N = int(input())
+    Arr = list(map(int,input().split()))
+    
+    if N % 2 == 0:
+        print('no')
+    else:
+        if Arr[0] != 1 or Arr[-1] != 1:
+            print("no")
+        else:
+            for i in range(N//2):
+                if Arr[i] != i+1:
+                    print('no')
+                    break
+            else:
+                Num= Arr[N//2]
+
+                for j in range(N//2,N):
+
+                    if Arr[j] != Num:
+                        print('no')
+                        break
+                    else:
+                        Num = Num - 1
+
+                else:
+                    print('yes')
